dd45875b6d0270bbc91bd078663f2413b4ddb63c
zhaohaoran <1036066414@qq.com>
2024-01-30 17:15:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# vite-vue-shadcn-starter
+
+```sh
+bun install
+bun run dev
+bunx shadcn-vue@latest add button
+```
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-screen">
+    <Button>Click me</Button>
+  </div>
+</template>
